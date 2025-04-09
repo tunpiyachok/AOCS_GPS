6802ff31ac53449e0540306b01f26c83655ebd42
tunpiyachok <piyachok.r@ku.th>
2025-04-10 02:21:17 +0700

    Update TC_GPS.c

--- TC_GPS.c
+++ TC_GPS.c
@@ -1,14 +1,15 @@
+/*
+Development by : Piyachok Ridsadaeng
+*/
+//Telecommand GPS monitoring
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <termios.h>
 #include <time.h>
-#include <stdlib.h>
 #include <mqueue.h>
 #include <sys/stat.h>
 #include <sys/resource.h>
@@ -19,6 +20,7 @@
 Message receive_msg = {0};
 Message send_msg = {0};
 
+// receive from AOCS shell and send command to GPS module
 void send(void*arg)
 {
     struct mq_attr attributes = {
@@ -27,25 +29,25 @@ void send(void*arg)
     	    .mq_curmsgs = 0,
     	    .mq_msgsize = sizeof(Message)
     	  };
-    mqd_t mq_telecommand = mq_open("/mq_tc_gps", O_CREAT | O_RDWR, S_IRWXU, &attributes);
+    mqd_t mq_telecommand = mq_open("/mq_tc_gps", O_CREAT | O_RDWR, S_IRWXU, &attributes);// receive from AOCS shell
     if (mq_telecommand == -1)
     {
         perror("mq_open()");
         exit(1);
     }
-    mqd_t mq_send_read = mq_open("/mq_telecommand_send_read", O_CREAT | O_RDWR, S_IRWXU, &attributes);
+    mqd_t mq_send_read = mq_open("/mq_telecommand_send_read", O_CREAT | O_RDWR, S_IRWXU, &attributes);// send to GPS module for sampling value
     if (mq_send_read == -1)
     {
         perror("mq_open()");
         exit(1);
     }
-    mqd_t mq_send_log = mq_open("/mq_telecommand_send_log", O_CREAT | O_RDWR, S_IRWXU, &attributes);
+    mqd_t mq_send_log = mq_open("/mq_telecommand_send_log", O_CREAT | O_RDWR, S_IRWXU, &attributes);// send to GPS module for log value
     if (mq_send_log == -1)
     {
         perror("mq_open()");
         exit(1);
     }
-    mqd_t mqdes_send = mq_open("/mq_receive_req", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, &attributes);
+    mqd_t mqdes_send = mq_open("/mq_receive_req", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, &attributes);// return to AOCS shell
     if (mqdes_send == -1) {
         perror("mq_open /mq_receive_req");
         exit(EXIT_FAILURE);
@@ -53,7 +55,7 @@ void send(void*arg)
 
     while (1)
     {
-        if (mq_receive(mq_telecommand, (char *)&receive_msg, sizeof(receive_msg), NULL) == -1)
+        if (mq_receive(mq_telecommand, (char *)&receive_msg, sizeof(receive_msg), NULL) == -1)// receive message from AOCS shell
         {
             perror("mq_receive()");
             exit(1);
@@ -66,7 +68,7 @@ void send(void*arg)
 	        printf("Module : %hhu\n",send_msg.mdid);
 	        printf("Request : %hhu\n",send_msg.req_id);
           printf("---------------------With for response----------------------\n\n");
-          if (mq_send(mq_send_read, (char *)&send_msg, sizeof(send_msg), 1) == -1) 
+          if (mq_send(mq_send_read, (char *)&send_msg, sizeof(send_msg), 1) == -1) // send message to GPS module for start sampling
     			{
     				perror("mq_send");
     				exit(EXIT_FAILURE);
@@ -79,7 +81,7 @@ void send(void*arg)
 	        printf("Module : %hhu\n",send_msg.mdid);
 	        printf("Request : %hhu\n",send_msg.req_id);
           printf("---------------------With for response----------------------\n\n");
-          if (mq_send(mq_send_read, (char *)&send_msg, sizeof(send_msg), 1) == -1) 
+          if (mq_send(mq_send_read, (char *)&send_msg, sizeof(send_msg), 1) == -1) // send message to GPS module for stop sampling
     			{
     				perror("mq_send");
     				exit(EXIT_FAILURE);
@@ -92,7 +94,7 @@ void send(void*arg)
 	        printf("Request : %hhu\n",send_msg.req_id);
 	        printf("Parameter. : %hhu\n",send_msg.param);
           printf("---------------------With for response----------------------\n\n");
-          if (mq_send(mq_send_log, (char *)&send_msg, sizeof(send_msg), 1) == -1) 
+          if (mq_send(mq_send_log, (char *)&send_msg, sizeof(send_msg), 1) == -1)  // send message to GPS module for start log
     			{
     				perror("mq_send");
     				exit(EXIT_FAILURE);
@@ -105,7 +107,7 @@ void send(void*arg)
 	        printf("Request : %hhu\n",send_msg.req_id);
 	        printf("Parameter : %hhu\n",send_msg.param);
           printf("---------------------With for response----------------------\n\n");
-          if (mq_send(mq_send_log, (char *)&send_msg, sizeof(send_msg), 1) == -1) 
+          if (mq_send(mq_send_log, (char *)&send_msg, sizeof(send_msg), 1) == -1) // send message to GPS module for stop log
     			{
     				perror("mq_send");
     				exit(EXIT_FAILURE);
@@ -120,13 +122,14 @@ void send(void*arg)
             printf("Return Parameter : %u\n",receive_msg.param);
             printf("[TC_GPS] Unknown request received.\n");
             printf("-------------------------------------------\n\n");
-            if (mq_send(mqdes_send, (char *)&receive_msg, sizeof(receive_msg), 1) == -1) {
+            if (mq_send(mqdes_send, (char *)&receive_msg, sizeof(receive_msg), 1) == -1) // return message to AOCS shell
+	    {
                 perror("mq_send /mq_receive_req");
                 exit(EXIT_FAILURE);
             }
 		  }
         
-        Message send_msg = {0};
+        	Message send_msg = {0};
 		Message receive_msg = {0};
     }
   	mq_close(mq_send_read);
@@ -139,7 +142,7 @@ void send(void*arg)
 
 }
 
-// ? TC_GPS.c - return_AOCS_shell()
+//receive from GPS module and return to AOCS shell
 void return_AOCS_shell(void* arg) {
     struct mq_attr attributes = {
         .mq_flags = 0,
@@ -148,13 +151,13 @@ void return_AOCS_shell(void* arg) {
         .mq_msgsize = sizeof(Message)
     };
 
-    mqd_t mq_return = mq_open("/mq_return", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, &attributes);
+    mqd_t mq_return = mq_open("/mq_return", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, &attributes);// receive from GPS module
     if (mq_return == -1) {
         perror("mq_open /mq_return");
         exit(EXIT_FAILURE);
     }
 
-    mqd_t mqdes_send = mq_open("/mq_receive_req", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, &attributes);
+    mqd_t mqdes_send = mq_open("/mq_receive_req", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, &attributes);// return to AOCS shell
     if (mqdes_send == -1) {
         perror("mq_open /mq_receive_req");
         exit(EXIT_FAILURE);
@@ -163,7 +166,7 @@ void return_AOCS_shell(void* arg) {
     while (1) {
         Message receive_msg = {0};
 
-        if (mq_receive(mq_return, (char *)&receive_msg, sizeof(receive_msg), NULL) == -1) {
+        if (mq_receive(mq_return, (char *)&receive_msg, sizeof(receive_msg), NULL) == -1) {// receive message from GPS module
             perror("mq_receive /mq_return");
             exit(EXIT_FAILURE);
         }
@@ -175,7 +178,7 @@ void return_AOCS_shell(void* arg) {
             printf("Return Parameter : %u\n",receive_msg.param);
             printf("-------------------------------------------\n\n");
 
-            if (mq_send(mqdes_send, (char *)&receive_msg, sizeof(receive_msg), 1) == -1) {
+            if (mq_send(mqdes_send, (char *)&receive_msg, sizeof(receive_msg), 1) == -1) {// return to AOCS shell
                 perror("mq_send /mq_receive_req");
                 exit(EXIT_FAILURE);
             }
@@ -192,7 +195,7 @@ void return_AOCS_shell(void* arg) {
 
 int main()
 {
-  pthread_t send_gps, return_shell;
+	pthread_t send_gps, return_shell;
 	
 	pthread_create(&send_gps, NULL, (void *(*)(void *))send, NULL);
 	pthread_create(&return_shell, NULL, (void *(*)(void *))return_AOCS_shell, NULL);
